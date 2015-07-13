1a9b70ebe1893af57e4156933ddeaaad08ea67fd
Will Alvarez <walvarez@simplifit.mx>
2015-07-13 10:19:51 -0500

    merge modelado crud branch to master

---
+++ application/views/micrositio/micro.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <title></title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="favicon.png" rel="icon" type="image/png">
+  </head>
+  <body>
+    <div class="container">Micrositio</div>
+    <script src="&lt;?php echo base_url('assets/javascripts/material.min.js'); ?&gt;"></script>
+  </body>
+</html>
