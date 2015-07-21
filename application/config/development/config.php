e5e78239ad3cf01d96dbc325a05b075cb2e3b165
Will Alvarez <walvarez@simplifit.mx>
2015-07-20 22:50:42 -0500

    micrositio ttf test

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://keypanel.dev/';
+$config['base_url'] = 'http://localhost:8080/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/micrositio/micro.php
+++ application/views/micrositio/micro.php
@@ -5,11 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="description" content="">
     <title></title>
-    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="/assets/css/materialize.css" media="screen,projection">
+    <link rel="stylesheet" href="/assets/css/custom.css" media="screen,projection">
     <link href="favicon.png" rel="icon" type="image/png">
   </head>
-  <body>
-    <div class="container">Micrositio</div>
-    <script src="&lt;?php echo base_url('assets/javascripts/material.min.js'); ?&gt;"></script>
+  <body id="micro">
+    <div class="container">
+      <nav>
+        <div class="nav-wrapper"><a href="#" class="brand-logo">Logo</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="/assets/js/materialize.min.js"></script>
   </body>
 </html>
