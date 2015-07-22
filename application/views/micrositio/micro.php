bc82bb11fd0656014d51f90eb21778cc22017449
Will Alvarez <walvarez@simplifit.mx>
2015-07-21 21:39:45 -0500

    micrositio ready css

--- application/views/micrositio/micro.php
+++ application/views/micrositio/micro.php
@@ -11,15 +11,85 @@
   </head>
   <body id="micro">
     <div class="container">
-      <nav>
-        <div class="nav-wrapper"><a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+      <nav class="header">
+        <div class="nav-wrapper"><a href="#" class="brand-logo"><img src="/assets/images/logo-micrositio.png" alt=""></a>
+          <ul id="nav-mobile" class="center-align">
+            <li class="active"><a href="sass.html">Inicio</a></li>
+            <li><a href="badges.html">Características del servicio</a></li>
+            <li><a href="collapsible.html">Contacto</a></li>
           </ul>
         </div>
       </nav>
+      <div class="row">
+        <div class="col sm12 m8 l8">
+          <div class="card">
+            <div class="card-image"><img src="images/" alt=""><span class="card-title">¿Qué es Key panel?</span></div>
+          </div>
+        </div>
+        <form id="login-card" class="col sm12 m4 l4">
+          <div class="card">
+            <div class="card-content">
+              <h6 class="blue-grey-text text-darken-4">Ingresa a tu KeyPanel</h6><a id="forgot-pass" href="#"> 
+                <p class="blue-grey-text text-lighten-3">¿Olvidaste tu clave?</p></a>
+              <div class="row">
+                <div class="input-field col s12 m12 l12">
+                  <input id="email" type="email" class="validate">
+                  <label for="email">Usuario</label>
+                </div>
+                <div class="input-field col s12 m12 l12">
+                  <input id="password" type="password" class="validate">
+                  <label for="password">Clave</label>
+                </div>
+              </div>
+              <div class="row">
+                <button type="submit" name="action" class="col s12 m12 l12 btn-large blue waves-effect waves-light">Ingresar</button>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div class="col sm12 m4 l4">
+          <div class="card small feature">
+            <div class="card-content">
+              <div class="row">
+                <h6 class="s12 m12 l12 blue-grey-text text-darken-4">Nuestro Servicio</h6>
+              </div>
+              <div class="row">
+                <p class="s12 m12 l12 blue-grey-text text-lighten-3">Conoce más de KeyPanel aquí.</p>
+              </div>
+            </div>
+            <div class="card-action">
+              <div class="row"><a id="knowmore" href="#" class="btn-large blue waves-effect waves-light col s12 m12 l12 text-white">CARACTERÍSTICAS</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col sm12 m4 l4">
+          <div class="card small feature">
+            <div class="card-content">
+              <div class="row">
+                <h6 class="col s12 m12 l12 blue-grey-text text-darken-4">Estadísticas</h6>
+              </div>
+              <div class="row">
+                <p class="col s12 m12 l12 blue-grey-text text-lighten-3">Más de 1,500 perfiles y 200 organigramas de los principales stakeholders públicos.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col sm12 m4 l4">
+          <div class="card small feature">
+            <div class="card-content">
+              <div class="row">
+                <h6 class="col s12 m12 l12 blue-grey-text text-darken-4">Solicita una prueba hoy</h6>
+              </div>
+              <div class="row">
+                <p class="col s12 m12 l12 blue-grey-text text-lighten-3">Prueba todo lo que KeyPanel puede hacer.</p>
+              </div>
+            </div>
+            <div class="card-action">
+              <div class="row"><a id="knowmore" href="#" class="btn-large blue waves-effect waves-light col s12 m12 l12 text-white">SOLICITA TU PRUEBA</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="/assets/js/materialize.min.js"></script>
