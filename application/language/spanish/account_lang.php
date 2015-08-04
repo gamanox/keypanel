7376bf275bb9a0f7883b8c4ea97ae4078395a3cd
Guillermo Lucio <guillermo.lucio@gmail.com>
2015-08-04 01:10:25 -0500

    Langs, commit anterior me los borro

---
+++ application/language/spanish/account_lang.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * Account lang
+     */
+
+    $lang['my-account-title']                 = 'Mi cuenta | KeyPanel';
+
+    $lang['card-noticias-title']              = 'Noticias';
+    $lang['card-historial-title']             = 'Historial';
+    $lang['card-organigramas-publicos-title'] = 'Públicos';
+    $lang['card-organigramas-publicos-desc']  = 'Información relevante sobre instituciones públicas de los diferentes poderes del Estado y Gobiernos Autónomos Descentralizados, sus autoridades y actores clave.';
+
+    $lang['card-organigramas-privados-title'] = 'Privados';
+    $lang['card-organigramas-privados-desc']  = 'Información relevante sobre organismos e instituciones privadas y otras entidades de interés, sus autoridades y actores clave.';
+    $lang['card-actualizaciones-title']       = 'Actualizaciones';
+    $lang['card-tendencias-title']            = 'Tendencias';
---
+++ application/language/spanish/errores_lang.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+     * Errores Lang
+     */
+
+    $lang['payment-required-title'] = 'Membresía no válida | KeyPanel';
+    $lang['session-expired-title']  = 'Sesión Expirada | KeyPanel';
+
+    $lang['error-sesion-expired']   = 'Sesión Expirada';
