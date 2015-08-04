df7f4e75ce1594e4caeb4dc1374fc49895cfdcec
Will Alvarez <walvarez@simplifit.mx>
2015-08-03 23:57:12 -0500

    panel-controller second deploy

---
+++ application/language/spanish/main_lang.php
@@ -0,0 +1,11 @@
+<?php 
+    /**
+     * Variables de lenguaje para el micrositio
+     */
+    
+    $lang['main-title']                = 'KeyPanel';
+    $lang['main-menu-inicio']          = 'Inicio';
+    $lang['main-menu-caracteristicas'] = 'Características del Servicio';
+    $lang['main-menu-contacto']        = 'Contacto';
+    
+    $lang['micrositio_form_title']     = 'Ingresa a tu KeyPanel';
