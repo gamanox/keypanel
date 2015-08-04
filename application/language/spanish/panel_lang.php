8fac6ccf9b61ab7c8348affd9b126191c1bd28d3
Will Alvarez <walvarez@simplifit.mx>
2015-08-04 00:55:33 -0500

    lang

---
+++ application/language/spanish/panel_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     * Panel Language
+     */
+
+    $lang['card-organigramas-publicos-title'] = 'Públicos';
+    $lang['card-organigramas-publicos-desc']  = 'Información relevante sobre instituciones públicas de los diferentes poderes del Estado y Gobiernos Autónomos Descentralizados, sus autoridades y actores clave.';
+
+    $lang['card-organigramas-privados-title'] = 'Privados';
+    $lang['card-organigramas-privados-desc']  = 'Información relevante sobre organismos e instituciones privadas y otras entidades de interés, sus autoridades y actores clave.';
+
+    $lang['card-historial-title']       = 'Historial';
+    $lang['card-noticias-title']        = 'Noticias';
+    $lang['card-actualizaciones-title'] = 'Actualizaciones';
+    $lang['card-tendencias-title']      = 'Tendencias';
