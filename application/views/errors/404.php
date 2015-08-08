75c3832eb7f31c579f51aefb20e80d2e1e40cefb
Will Alvarez <walvarez@simplifit.mx>
2015-08-08 12:57:25 -0500

    404 php

---
+++ application/views/errors/404.php
@@ -0,0 +1,12 @@
+
+<div class="container error">
+  <div class="col m12 page-404">
+    <div class="number">404</div>
+    <div class="details">
+      <h3>Oops! Parece que estás perdido.</h3>
+      <p>No encontramos la página que buscabas.<br/><a href="#">Regresa al inicio</a>
+        <!-- /input-group-->
+      </p>
+    </div>
+  </div>
+</div>
