291c5d0ce8690392ee9e36e82b8ae14bbc7b6f35
Will Alvarez <walvarez@simplifit.mx>
2015-08-08 20:52:21 -0500

    merge branch luis a panel controller

--- application/config/development/config.php
+++ application/config/development/config.php
@@ -17,7 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://keypanel.dev/';
+// $config['base_url'] = 'http://keypanel.dev/';
+$config['base_url'] = 'http://localhost:8080/';
 
 /*
 |--------------------------------------------------------------------------
